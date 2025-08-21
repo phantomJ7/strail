8b14027c0833c88046a35d46dbe43c621d370a78
phantomJ7 <127464183+phantomJ7@users.noreply.github.com>
2025-08-21 16:19:30 +0530

    Update App.controller.js

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,1 +1,17 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+  "use strict";
+
+  return Controller.extend("ui5.walkthrough.controller.App", {
+    onShowHello: function () {
+      // read i18n resource bundle
+      const oBundle = this.getView().getModel("i18n").getResourceBundle();
+      const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+      const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+      MessageToast.show(sMsg);
+    }
+  });
+});
 
